645f1b7494370268244b0c8f2bcb498387bd0db5
Marques Avery <averymarques4@gmail.com>
2020-07-29 19:59:52 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(say = "Squawk!", parrot_says = "Pretty bird!")
-#  puts "#{say}"
-  puts "#{parrot_says}"
+  puts "#{say}"
+
 end
